c6f6d7cab694ff936ba7a70f83c92c05990888bf
huawangxin <wangge703@163.com>
2016-11-10 14:53:48 +0800

    小更

--- src/random/Choujiang.java
+++ src/random/Choujiang.java
@@ -4,6 +4,7 @@ package src.random;
  * 简单抽奖线程
  * @author huawangxin
  *
+ * math.random()是令系统随机选取大于等于 0.0 且小于 1.0 的伪随机 double 值
  * math.ceil(x)返回大于参数x的最小整数,即对浮点数向上取整
  * @date 2016年11月10日 下午2:41:36
  */
