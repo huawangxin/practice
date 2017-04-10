187d3f120f552cad24f7c979ca2e71e272f4720e
huawangxin <wangge703@163.com>
2017-04-10 08:54:49 +0800

    基数排序

--- src/sort/SortUtils.java
+++ src/sort/SortUtils.java
@@ -1,5 +1,8 @@
 package src.sort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 排序工具类
  * @author huawangxin
@@ -230,5 +233,56 @@ public class SortUtils {
 	        data[tmp]=tmpArr[tmp++];  
 	    }
 		return data;
+	}
+	/**
+	 * 基数排序
+	 * 
+	 * 基本思想：将所有待比较数值（正整数）统一为同样的数位长度，数位较短的数前面补零。
+	 * 然后，从最低位开始，依次进行一次排序。这样从最低位排序一直到最高位排序完成以后,数列就变成一个有序序列。
+	 * @return
+	 * @huawangxin
+	 */
+	public static int[] radixSort(int[] num) {
+		// 首先确定排序的趟数;
+		int max = num[0];
+		for (int i = 1; i < num.length; i++) {
+			if (num[i] > max) {
+				max = num[i];
+			}
+		}
+		int time = 0;
+		// 判断位数;
+		while (max > 0) {
+			max /= 10;
+			time++;
+		}
+		// 建立10个队列;
+		List<ArrayList> queue = new ArrayList<ArrayList>();
+		for (int i = 0; i < 10; i++) {
+			ArrayList<Integer> queue1 = new ArrayList<Integer>();
+			queue.add(queue1);
+		}
+		// 进行time次分配和收集;
+		for (int i = 0; i < time; i++) {
+			// 分配数组元素;
+			for (int j = 0; j < num.length; j++) {
+				// 得到数字的第time+1位数;
+				int x = num[j] % (int) Math.pow(10, i + 1) / (int) Math.pow(10, i);
+				ArrayList<Integer> queue2 = queue.get(x);
+				queue2.add(num[j]);
+				queue.set(x, queue2);
+			}
+			int count = 0;// 元素计数器;
+			// 收集队列元素;
+			for (int k = 0; k < 10; k++) {
+				while (queue.get(k).size() > 0) {
+					ArrayList<Integer> queue3 = queue.get(k);
+					num[count] = queue3.get(0);
+					queue3.remove(0);
+					count++;
+				}
+			}
+		}
+		return num;
 	}
 }
